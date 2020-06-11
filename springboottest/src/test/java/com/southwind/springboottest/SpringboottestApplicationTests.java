2c753f49d4c1ea560c27c042bc7a583da8490579
Wan Liu <liuwan866@gmail.com>
2020-06-11 10:56:36 +0200

    second commit

--- springboottest/src/test/java/com/southwind/springboottest/SpringboottestApplicationTests.java
+++ springboottest/src/test/java/com/southwind/springboottest/SpringboottestApplicationTests.java
@@ -1,13 +1,37 @@
 package com.southwind.springboottest;
 
+import com.southwind.springboottest.entity.Book;
+import com.southwind.springboottest.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 @SpringBootTest
 class SpringboottestApplicationTests {
+    @Autowired
+    private BookRepository repository;
 
     @Test
     void contextLoads() {
+        PageRequest pageRequest = PageRequest.of(0,6);
+        Page<Book> page = repository.findAll(pageRequest);
+        int i = 0;
     }
 
+
+
+    @Test
+    void findById(){
+        Book book = repository.findById(1).get();
+        System.out.println(book);
+    }
+
+
+
+    @Test
+    void delete(){
+        repository.deleteById(1);
+    }
 }
